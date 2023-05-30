628c12ce3b442d1615b564b03dd577b59480916d
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-05-30 07:11:55 +0200

    Add project files.

---
+++ AutoCAD Plugins and Addons/LayoutLister.cs
@@ -0,0 +1,26 @@
+﻿
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using System;
+
+namespace Test_329.NET_4._8
+{
+    public class LayoutLister
+    {
+        [CommandMethod("ListLayouts")]
+        public void ListLayouts()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            using (Transaction tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                DBDictionary layouts = (DBDictionary)tr.GetObject(doc.Database.LayoutDictionaryId, OpenMode.ForRead);
+                Console.WriteLine("\nLayout names: ");
+                foreach (DBDictionaryEntry entry in layouts)
+                {
+                    Console.WriteLine(entry.Key);
+                }
+            }
+        }
+    }
+}
